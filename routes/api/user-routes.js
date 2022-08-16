ef59c3fd71e30fe3542e7a259cbbaeb1340605b0
swvmpdad <will.g.camp@gmail.com>
2022-08-16 18:02:40 -0500

    added login functionality

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
   
 // GET /api/users/1
 router.get('/:id', (req, res) => {
@@ -33,7 +33,7 @@ router.get('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-  });
+});
 
 // POST /api/users
 router.post('/', (req, res) => {
@@ -48,7 +48,30 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+router.post('/login', (req, res) => {
+  // expects {email: 'swvmpdad', password: 'password123'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorret password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!!' });
   });
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
@@ -92,6 +115,6 @@ router.delete('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
 
 module.exports = router;
