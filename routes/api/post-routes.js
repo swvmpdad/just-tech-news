dde050457a803f59e89acf322933ede94304d934
swvmpdad <will.g.camp@gmail.com>
2022-08-18 16:25:38 -0500

    added Comment model and routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
-// get all users
+// get all posts
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
@@ -16,8 +16,16 @@ router.get('/', (req, res) => {
         order: [['created_at', 'DESC']],
         include: [
             {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
                 model: User,
                 attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
             }
         ]
     })
